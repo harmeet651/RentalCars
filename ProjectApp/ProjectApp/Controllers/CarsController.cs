c6ffcaf4eb35531f3c1daf519a1b9bb8431ddd65
Harmeet Singh <harmeet651@gmail.com>
2019-07-09 10:39:29 -0500

    sql data received

--- ProjectApp/ProjectApp/Controllers/CarsController.cs
+++ ProjectApp/ProjectApp/Controllers/CarsController.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Data;
 
 namespace ProjectApp.Controllers
 {
@@ -13,6 +16,29 @@ namespace ProjectApp.Controllers
         // GET: Cars
         public ActionResult Index(int? pageIndex, string sortBy)
         {
+            string connectionString = "Data Source=52.189.181.41 ;Initial Catalog=Sample; User ID=sa;Password=Lb@zxc)0";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                //SqlCommand cmd = new SqlCommand("select * from CustomerCar", con);
+                //con.Open();
+                SqlConnection objConn = new SqlConnection(connectionString);
+                objConn.Open();
+                SqlDataAdapter daAuthors = new SqlDataAdapter("Select * From CustomerCar", objConn);
+                DataSet dsPubs = new DataSet("Pubs");
+                daAuthors.FillSchema(dsPubs, SchemaType.Source, "Authors");
+                daAuthors.Fill(dsPubs, "Authors");
+
+                DataTable tblAuthors;
+                tblAuthors = dsPubs.Tables["Authors"];
+
+                //foreach (DataRow drCurrent in tblAuthors.Rows)
+                //{
+                //    Console.WriteLine("{0} {1}",
+                //    drCurrent["au_fname"].ToString(),
+                //    drCurrent["au_lname"].ToString());
+                //}
+                Console.ReadLine();
+            }
             var cars = GetCars();
             return View(cars);
         }
---
+++ ProjectApp/ProjectApp/DB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ProjectApp
+{
+    public class DB
+    {
+        static void Main()
+        {
+            string connectionString ="Data Source=52.189.181.41 ;Initial Catalog=Sample; Integrated Security=SPSI";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("select * from CustomerCar", con);
+                con.Open();
+                Console.WriteLine(cmd);
+            }
+        }
+    }
+}
