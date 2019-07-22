2597c3d2e024c2b61c7dc2a4a6e61a24289f85b0
Harmeet Singh <harmeet651@gmail.com>
2019-07-22 17:10:36 -0500

    fb, https filter

--- ProjectApp/ProjectApp/App_Start/FilterConfig.cs
+++ ProjectApp/ProjectApp/App_Start/FilterConfig.cs
@@ -9,6 +9,7 @@ namespace ProjectApp
         {
             filters.Add(new HandleErrorAttribute());
             //filters.Add(new AuthorizeAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
